f975e5568112027e703d4ded2ab5c76d413c5a42
Tiiwoo <tiiwoo.taowei@outlook.com>
2023-12-24 23:07:54 +0800

    feat: add dockerfile

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"Rules/build"
 	"fmt"
+	"log"
+	"net/http"
 	"sync"
 )
 
@@ -16,4 +18,16 @@ func main() {
 	wg.Wait()
 	build.BuildPublic()
 	fmt.Println("Build done!")
+	// 设置静态文件服务的根目录
+	fs := http.FileServer(http.Dir("./"))
+
+	// 将根URL路径（"/"）映射到文件服务器
+	http.Handle("/", fs)
+
+	// 启动HTTP服务器并监听8080端口
+	log.Println("Listening on :8080...")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
