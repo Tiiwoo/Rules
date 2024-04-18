248bae654f4b7499fa73332dd3e6e5350e9b4749
Tiiwoo <Tiiwoo@users.noreply.github.com>
2024-04-18 14:37:45 +0000

    deploy: a600ef187511fab8cde18d901a30fe80c0efa213

---
+++ Script/aptakube.js
@@ -0,0 +1,17 @@
+let url = $request.url;
+let activate = () => {
+  if (url !== 'https://aptakube.com/api/v1/trial/validate') return;
+  let body = JSON.stringify({
+    success: true,
+    response: {
+      expiry_date: '2035-06-09T23:59:59.000Z',
+      token:
+        'eyJhbGciOiJIUzUxMiJ9eyJleHBpcnlfZGF0ZSI6IjIwMjMtMDktMjBUMjM6NTk6NTkuMDAwWiIsImlhdCI6MTY5MjY4ODU2MywiaXNzIjoiYXB0YWt1YmUtY29tIiwiZXhwIjoxNzkyOTk4NTYzfQ==',
+    },
+  });
+  $done({
+    response: {
+      body,
+    },
+  });
+};
