8f5560474763ba82954d64e5f3a8dd172e33aa8a
Tiiwoo <tiiwoo.taowei@outlook.com>
2024-04-19 00:01:42 +0800

    feat: update

--- Script/aptakube.js
+++ Script/aptakube.js
@@ -2,11 +2,10 @@ let url = $request.url;
 let activate = () => {
   if (url !== 'https://aptakube.com/api/v1/license/validate') return;
   let body = JSON.stringify({
-    success: true,
-    data: {
-      expiry_date: '2035-06-09',
-      token: 'aaaaa',
-    },
+    license_key: 'V2PCP-W8OSZ-BTM2Y-C5X42-RAKW0-XUEHU',
+    token:
+      'eyJhbGciOiJIUzUxMiJ9.eyJleHBpcnlfZGF0ZSI6IjIwMzctMTItMzFUMjM6NTk6NTkuMDAwWiIsImxpY2Vuc2Vfa2V5IjoiVjJQQ1AtVzhPU1otQlRNMlktQzVYNDItUkFLVzAtWFVFSFUiLCJpYXQiOjE3MTM0NTU5NjgsImlzcyI6ImFwdGFrdWJlLWNvbSIsImV4cCI6MTcxMzYyODc2OH0.8St_GD9zNLuBPIhQbuQOrR73Npe2Fc_t9hjzhWSsuZE4waoyPjvg-C1fLxZYFxa1TDwdcUVny2lFHvq23LEEbg',
+    expiry_date: '2037-12-31T23:59:59.000Z',
   });
   $done({
     response: {
