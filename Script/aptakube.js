e48148719e3217112303e9eea16b3e6a3b371132
Tiiwoo <tiiwoo.taowei@outlook.com>
2024-04-18 23:12:52 +0800

    feat: update

--- Script/aptakube.js
+++ Script/aptakube.js
@@ -4,9 +4,8 @@ let activate = () => {
   let body = JSON.stringify({
     success: true,
     response: {
-      expiry_date: '2035-06-09T23:59:59.000Z',
-      token:
-        'eyJhbGciOiJIUzUxMiJ9eyJleHBpcnlfZGF0ZSI6IjIwMjMtMDktMjBUMjM6NTk6NTkuMDAwWiIsImlhdCI6MTY5MjY4ODU2MywiaXNzIjoiYXB0YWt1YmUtY29tIiwiZXhwIjoxNzkyOTk4NTYzfQ==',
+      expiry_date: '2035-06-09',
+      token: 'aaaaa',
     },
   });
   $done({
