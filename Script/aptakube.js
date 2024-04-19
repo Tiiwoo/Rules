f016ca411c59566c675f3bd1928d5ec80aad0574
Tiiwoo <tiiwoo.taowei@outlook.com>
2024-04-19 08:59:22 +0800

    feat: update

--- Script/aptakube.js
+++ Script/aptakube.js
@@ -5,7 +5,7 @@ let activate = () => {
     license_key: 'V2PCP-W8OSZ-BTM2Y-C5X42-RAKW0-XUEHU',
     token:
       'eyJhbGciOiJIUzUxMiJ9.eyJleHBpcnlfZGF0ZSI6IjIwMzctMTItMzFUMjM6NTk6NTkuMDAwWiIsImxpY2Vuc2Vfa2V5IjoiVjJQQ1AtVzhPU1otQlRNMlktQzVYNDItUkFLVzAtWFVFSFUiLCJpYXQiOjE3MTM0NTU5NjgsImlzcyI6ImFwdGFrdWJlLWNvbSIsImV4cCI6MTcxMzYyODc2OH0.8St_GD9zNLuBPIhQbuQOrR73Npe2Fc_t9hjzhWSsuZE4waoyPjvg-C1fLxZYFxa1TDwdcUVny2lFHvq23LEEbg',
-    expiry_date: '2037-12-31T23:59:59.000Z',
+    expiry_date: '2099-12-31T23:59:59.000Z',
   });
   $done({
     response: {
